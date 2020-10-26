b7ec125ba3881890764b09f452d924bbda6a0a42
Dmitriy Khromov <devandart@ya.ru>
2020-10-27 02:19:11 +0300

    rspec - registration with out terms

--- spec/controlers/registrations_controller_spec.rb
+++ spec/controlers/registrations_controller_spec.rb
@@ -21,6 +21,29 @@ describe Api::V1::RegistrationsController, type: :controller do
         end
       end
 
+      context 'without terms' do
+        let(:pas) { 'example_password' }
+
+        let(:user_params) { { email: 'email@example.com',
+                              nationality: ISO3166::Country.all_translated('EN').sample,
+                              country: ISO3166::Country.all_translated('EN').sample,
+                              password: pas,
+                              password_confirmation: pas } }
+
+        it "should not be created user" do
+          @request.env["devise.mapping"] = Devise.mappings[:user]
+
+          pattern = {
+              agree_with_terms: ["is not included in the list"],
+              aml_rules: ["is not included in the list"],
+              over_18: ["is not included in the list"]
+          }
+
+          post :create, params: { user: user_params }
+          expect(response.body).to match_json_expression(pattern)
+        end
+      end
+
       context 'with empty params' do
         it "return validation errors" do
           @request.env["devise.mapping"] = Devise.mappings[:user]
